d43b052c6ed092d275b1b3a7b0ecbd58f0d3ad22
kingze1l <164335559+kingze1l@users.noreply.github.com>
2024-10-14 13:48:48 +1300

    change

--- 14.10.24.cpp
+++ 14.10.24.cpp
@@ -9,7 +9,6 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
+    std::cout << "Hello v vddfbfdbfdbdbdbgdbgd\n";
 }
 
